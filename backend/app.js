a208e08bc7ebd5610d1521a037a10e62d0f0aba2
ankit mishra <mishraankit987@gmail.com>
2023-08-30 14:54:06 +0530

    third

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express=require('express')
 const userroute=require('./route/userroute')
 const cookieparser=require('cookie-parser')
+const cors=require('cors')
 //mongoose connection import
 const ConnectToDb=require('./Config/db')
 ConnectToDb()
@@ -8,5 +10,8 @@ const app=express()
 //middleware
 app.use(express.json())
 app.use(cookieparser())
+app.use(cors({
+    origin:process.env.CLIENT_URL
+}))
 app.use('/',userroute)
 module.exports=app;
