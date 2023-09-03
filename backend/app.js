3267aedac712700497d9b6938ae33a4fa6845d99
ankit mishra <mishraankit987@gmail.com>
2023-09-03 16:47:03 +0530

    fifth

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,7 @@ const ConnectToDb=require('./Config/db')
 ConnectToDb()
 const app=express()
 //middleware
-app.use(cors({
-    origin:true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(cookieparser())
 
